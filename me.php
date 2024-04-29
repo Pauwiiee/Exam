4d8df2447cc0453d5541bac81bbae9b7ec360f46
Pauwiiee <158147759+Pauwiiee@users.noreply.github.com>
2024-04-29 14:20:52 +0800

    Add files via upload

---
+++ me.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MY FINAL EXAM</title>
+	<link rel="Stylesheet" type="text/css" href="me.css">
+</head>
+<body>
+	<div class="container">
+	<div class="letters>">
+	<div class="board">
+	<div class="circle1">
+	<div class="circle2">
+	<div class="braket">
+	<div class="braket2">
+	<div class="port">
+	<div class="pin">
+	<!--FAN DIVISION-->
+	<div class="fan1">
+	 <img src="Fans.png" alt="fans" id="fan-image">
+	</div>
+	<div class="fan2">
+	 <img src="Fans.png" alt="fans" id="fan-image2">
+	</div>
+	<div class="triangle"></div>
+	</div>
+	</div>
+</html>
